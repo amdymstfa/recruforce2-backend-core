68dbe3663bd3db080cc0fc5fddc1aefc66845208
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:21:56 +0000

    chore: add ContractType enum

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/ContractType.java
@@ -0,0 +1,15 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Types of employment contracts associated with job offers.
+ */
+public enum ContractType {
+
+  CDI,
+
+  CDD,
+
+  FREELANCE,
+
+  INTERNSHIP
+}
