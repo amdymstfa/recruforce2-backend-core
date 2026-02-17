0c8f9a900ba7338a417c719d6f07d3c2a3bd8ec8
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:21:56 +0000

    chore: add InterviewType enum

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/InterviewType.java
@@ -0,0 +1,11 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Defines the two interview phases in the RecruForce2 recruitment process.
+ */
+public enum InterviewType {
+
+  SOFT_SKILLS,
+
+  HARD_SKILLS
+}
