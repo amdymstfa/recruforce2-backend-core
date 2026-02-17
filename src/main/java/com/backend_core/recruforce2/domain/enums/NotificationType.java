323cca0c7d3317e7b237b05d2176f62e9c0ce1a2
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:21:56 +0000

    chore: add NotificationType enum

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/NotificationType.java
@@ -0,0 +1,19 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Types of notifications sent to users in the RecruForce2 platform.
+ */
+public enum NotificationType {
+
+  NEW_APPLICATION,
+
+  INTERVIEW_SCHEDULED,
+
+  INTERVIEW_REMINDER,
+
+  FEEDBACK_REQUIRED,
+
+  DEADLINE_APPROACHING,
+
+  STATUS_CHANGED
+}
