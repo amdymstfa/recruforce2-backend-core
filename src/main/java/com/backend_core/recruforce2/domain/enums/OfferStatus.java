51cc85cbc9734382b208d2189e9dcf123db834da
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:21:56 +0000

    chore: add OfferStatus enum

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/OfferStatus.java
@@ -0,0 +1,15 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Lifecycle statuses of a job offer.
+ */
+public enum OfferStatus {
+
+  DRAFT,
+
+  ACTIVE,
+
+  ARCHIVED,
+
+  EXPIRED
+}
