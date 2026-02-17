71f1223e8da4edf4af863e4089517b45f1065e35
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:11:39 +0000

    chore: create Weekday enum availability scheduling

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/WeekDay.java
@@ -0,0 +1,8 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Days of the week used for recruiter/manager availability scheduling
+ */
+public enum WeekDay {
+  MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+}
