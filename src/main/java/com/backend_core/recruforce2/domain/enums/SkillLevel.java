1179b9600c23c489e858f122fb4ca58df56a6d6b
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:21:56 +0000

    chore: add SkillLevel enum

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/SkillLevel.java
@@ -0,0 +1,15 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Proficiency levels for candidate skills.
+ */
+public enum SkillLevel {
+
+  BEGINNER,
+
+  INTERMEDIATE,
+
+  ADVANCED,
+
+  EXPERT
+}
