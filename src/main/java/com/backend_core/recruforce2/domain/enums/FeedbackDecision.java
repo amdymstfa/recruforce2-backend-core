8d62fcac1d93570332517c2d44098df4d7515b12
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 15:21:56 +0000

    chore: add FeedbackDecision enum

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/FeedbackDecision.java
@@ -0,0 +1,17 @@
+package com.backend_core.recruforce2.domain.enums;
+
+/**
+ * Final decision options available to an interviewer after completing an interview.
+ */
+public enum FeedbackDecision {
+
+  STRONGLY_RECOMMENDED,
+
+  RECOMMENDED,
+
+  NEUTRAL,
+
+  NOT_RECOMMENDED,
+
+  STRONGLY_REJECTED
+}
