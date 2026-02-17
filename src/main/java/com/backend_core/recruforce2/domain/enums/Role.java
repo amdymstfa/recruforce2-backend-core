c9017489af55e18a1a42b605229ccb101ded54f4
amdymstfa <amdymoustapha011@gmail.com>
2026-02-17 14:29:14 +0000

    chore: create Role enum with Admin, Recruiter, manager

---
+++ src/main/java/com/backend_core/recruforce2/domain/enums/Role.java
@@ -0,0 +1,13 @@
+package com.backend_core.recruforce2.domain.enums ;
+
+/**
+ * Define role available
+ */
+public enum Role {
+  /** Full access */
+  ADMIN,
+  /** manage job offer, application, interview and feedback */
+  RECRUITER,
+  /** View candidates and submit interview */
+  MANAGER
+}
