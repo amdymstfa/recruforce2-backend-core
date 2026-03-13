9cdf4f2a278dea15b07e0bcf2df7d77997cf9a5c
amdymstfa <amdymoustapha011@gmail.com>
2026-03-13 11:56:16 +0000

    feat(dto): add ExperienceResponse DTO

---
+++ src/main/java/com/backend_core/recruforce2/dto/response/ExperienceResponse.java
@@ -0,0 +1,22 @@
+package com.backend_core.recruforce2.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExperienceResponse {
+  private Long id;
+  private String position;
+  private String company;
+  private String description;
+  private LocalDate startDate;
+  private LocalDate endDate;
+  private Boolean isCurrent;
+}
